dac21eda8f879bb7856e0cf30fa1b4790f59bf9b
fredericklu <fredlu@yeah.net>
2017-05-02 10:21:14 +0200

    initial cnnmodel
    initial cnnmodel

--- video_level_models.py
+++ video_level_models.py
@@ -30,7 +30,7 @@ flags.DEFINE_integer(
 class LogisticModel(models.BaseModel):
   """Logistic model with L2 regularization."""
 
-  def create_model(self, model_input, vocab_size, l2_penalty=1e-8, **unused_params):
+  def create_model(self, model_input, vocab_size, **unused_params):
     """Creates a logistic model.
 
     Args:
@@ -43,7 +43,7 @@ class LogisticModel(models.BaseModel):
       batch_size x num_classes."""
     output = slim.fully_connected(
         model_input, vocab_size, activation_fn=tf.nn.sigmoid,
-        weights_regularizer=slim.l2_regularizer(l2_penalty))
+        weights_regularizer=slim.l2_regularizer(0.01))
     return {"predictions": output}
 
 class MoeModel(models.BaseModel):
@@ -53,7 +53,7 @@ class MoeModel(models.BaseModel):
                    model_input,
                    vocab_size,
                    num_mixtures=None,
-                   l2_penalty=1e-8,
+                   l2_penalty=1e-5,
                    **unused_params):
     """Creates a Mixture of (Logistic) Experts model.
 
@@ -101,3 +101,71 @@ class MoeModel(models.BaseModel):
     final_probabilities = tf.reshape(final_probabilities_by_class_and_batch,
                                      [-1, vocab_size])
     return {"predictions": final_probabilities}
+
+class CNNModel(models.BaseModel):
+
+  def create_model(self, model_input, vocab_size, l2_penalty=1e-8, **unused_params):
+
+    # Input Layer
+    input_layer = tf.reshape(model_input, [-1, 32, 32, 3])
+
+
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=96,
+      kernel_size=[7, 7],
+      strides=(2, 2),
+      padding="same",
+      activation=tf.nn.relu)
+
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=256,
+      kernel_size=[5, 5],
+      strides=(2, 2),
+      padding="same",
+      activation=tf.nn.relu)
+
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    conv3 = tf.layers.conv2d(
+      inputs=pool2,
+      filters=512,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+    conv4 = tf.layers.conv2d(
+      inputs=conv3,
+      filters=512,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+    conv5 = tf.layers.conv2d(
+      inputs=conv4,
+      filters=512,
+      kernel_size=[3, 3],
+      padding="same",
+      activation=tf.nn.relu)
+
+    pool3 = tf.layers.max_pooling2d(inputs=conv5, pool_size=[2, 2], strides=2)
+
+    pool3_flat = tf.reshape(pool3, [-1, 7 * 7 * 64])
+    dense = tf.layers.dense(inputs=pool3_flat, units=4096, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == learn.ModeKeys.TRAIN)
+    dense2 = tf.layers.dense(inputs=dropout, units=2048, activation=tf.nn.relu)
+    dropout2 = tf.layers.dropout(
+      inputs=dense2, rate=0.4, training=mode == learn.ModeKeys.TRAIN)
+
+    logits = tf.layers.dense(inputs=dropout2, units=10)
+
+
+    # output = slim.fully_connected(
+    #     model_input, vocab_size, activation_fn=tf.nn.sigmoid,
+    #     weights_regularizer=slim.l2_regularizer(l2_penalty))
+    return {"predictions": output}
