19b4ebdc07f5f88b6110eac59593fa1f9c1d08ff
Felix van Doorn <felix.a.vandoorn@gmail.com>
2017-05-22 21:21:49 +0200

    Other attempt for maxData

--- autoencoderLabels.py
+++ autoencoderLabels.py
@@ -33,7 +33,7 @@ def convert(label):
     return output
 
 
-maxData = 500000 # change this to maximum possible value
+maxData = 400000 # change this to maximum possible value
 trX=data['labels'][0:maxData].apply(convert)
 trX.names = ['labels']
 trX = np.matrix(trX.tolist())
